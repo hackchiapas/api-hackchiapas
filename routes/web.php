5ce04b0dd920d672bb8c70690e60fc4f7394289c
AugustoRuizC <163191@ids.upchiapas.edu.mx>
2018-08-30 22:08:16 -0500

    Se añadio nuevas rutas

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/name/get', 'ForgotNameController@showPageForgotName')->name('name.request');
-Route::post('/name/get', 'ForgotNameController@showPageForgotName')->name('name.email');
+Route::post('/name/get', 'ForgotNameController@sendEmail')->name('name.email');
