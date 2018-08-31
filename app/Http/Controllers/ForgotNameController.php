799a65a88dc4aaa197f629c261de7e8dbba1e0bc
AugustoRuizC <163191@ids.upchiapas.edu.mx>
2018-08-30 22:05:25 -0500

    Creacion de controller para enviar correo

--- app/Http/Controllers/ForgotNameController.php
+++ app/Http/Controllers/ForgotNameController.php
@@ -1,28 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-namespace Illuminate\Auth\Notifications;
 
 use Illuminate\Http\Request;
-use Illuminate\Notifications\Notification;
+use App\Mail\TestEmail;
 
-class ForgotNameController extends Notification{
-    public $token;
-
-    public function __construct($token){
-       $this->token = $token;
-    }
-
+class ForgotNameController extends Controller
+{
     public function showPageForgotName (){
         return view ('auth.name.email');
     }
-    public function via($notifiable){
-        return ['mail'];
-    }
-    public function message($notifiable)
-    {
-        return $this->line('You are receiving this email because we received a password reset request for your account. Click the button below to reset your password:')
-                    ->action('Reset Password', url('password/reset', $this->token).'?email='.urlencode($notifiable->email))
-                    ->line('If you did not request a password reset, no further action is required.');
+
+    public function sendEMail(){
+        $data = ['message' => 'This is a test!'];
+
+        \Mail::to('augustorucle@gmail.com')->send(new TestEmail($data));
+
+        return view('auth.name.email');
     }
 }
