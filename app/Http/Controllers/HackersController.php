c437fbb0365c93a831f14edb25d6d39301974831
AugustoRuizC <163191@ids.upchiapas.edu.mx>
2018-09-01 01:33:06 -0500

    Cambios no relevantes

--- app/Http/Controllers/HackersController.php
+++ app/Http/Controllers/HackersController.php
@@ -4,36 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Hacker;
+use App\User;
 
-class HackersController extends Controller
-{
-    public function select() 
-    {
-      $hackers = Hacker::all();
-    	return view('home', compact("hackers"));
-    }
+class HackersController extends Controller{
 
-    public function insert(Request $request) 
-    {
-    	$datos_hacker = $request->all();
-    	$datos_hacker["edad"] = intval($datos_hacker["edad"]);
-    	$datos_hacker["confirmado"] = boolval($datos_hacker["confirmado"]);
+  public function __construct(){
+    $this->middleware('auth');
+  }
+  
+  public function select() {
+    $Hackers = Hacker::all();
+    return view('home', ['Hackers' => $Hackers]);
+  }
+
+  public function insert(Request $request) {
+   	$datos_hacker = $request->all();
+   	$datos_hacker["edad"] = intval($datos_hacker["edad"]);
+   	$datos_hacker["confirmado"] = boolval($datos_hacker["confirmado"]);
 		
 		$hacker = Hacker::create($datos_hacker);
 		
 		return $hacker->id;
-    }
+  }
 
-    public function delete($id) 
-    {
-    	$hacker = Hacker::find($id);
-		$hacker->delete();
-    }
+  public function delete($id)   {
+   	$hacker = Hacker::find($id);
+	  $hacker->delete();
+  }
 
-    public function updateConfirmado($id, $value) 
-    {
-    	$hacker = Hacker::find($id);
-    	$hacker->confirmado = $value;
-		$hacker->save();
-    }
+  public function updateConfirmado($id, $value) {
+   	$hacker = Hacker::find($id);
+   	$hacker->confirmado = $value;
+	  $hacker->save();
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
 Route::get('/name/get', 'ForgotNameController@showPageForgotName')->name('name.request');
 Route::post('/name/get', 'ForgotNameController@sendEmail')->name('name.email');
 
