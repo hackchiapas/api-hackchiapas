57547a4d42f49dcb06433c84fcb3b7ffdadec06c
AugustoRuizC <163191@ids.upchiapas.edu.mx>
2018-09-01 19:05:44 -0500

    View admin terminada

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,62 +1,52 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Bienvenido administrador</div>
-            </div>
-            <table class="table">
-            
-                <thead class="thead-dark" style="background-color: #212121">
-                    <td style="background-color: #00C853; color: #FAFAFA" colspan="3">Lista de alumnos confirmados</td>
-                    <tr>
-                        <th scope="col" style="color: #FAFAFA"> id</th>
-                        <th scope="col" style="color: #FAFAFA" > name</th>
-                        <th scope="col" style="color: #FAFAFA"> email </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @php ($NotUser = "") @endphp
-                    @foreach($Hackers as $user)
-                        @if(($user->confirmado) === 1)
-                            <tr>
-                                <th scope="row"> {{$user->id}} </th>
-                                <td> {{$user->nombre}} </td>
-                                <td> {{$user->email}} </td>
-                            </tr>
-                        @else
-                            @php ($NotUser = $NotUser.$user->id.",".$user->nombre.",".$user->email."]") @endphp
-                        @endif
-                    @endforeach
-                </tbody>            
-            </table>
 
-            <table class="table" style="margin-top: 50px;">
-                <thead class="thead-dark" style="background-color: #212121">
-                    <td style="background-color: #D50000; color: #FAFAFA" colspan="3">Lista de alumnos sin confirmar</td>
-                    <tr>
-                        <th scope="col" style="color: #FAFAFA"> id</th>
-                        <th scope="col" style="color: #FAFAFA" > name</th>
-                        <th scope="col" style="color: #FAFAFA"> email </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @php 
-                        $arrayNotUser = explode ("]", $NotUser); 
-                    @endphp
-                    @for ($i = 0; $i < count( $arrayNotUser) - 1; $i++)
-                        @php  $data = explode(",", $arrayNotUser[$i]); @endphp
-                        <tr>
-                            <th scope="row"> {{ $data[0] }} </th>
-                            <td> {{ $data[1] }} </td>
-                            <td> {{ $data[2] }} </td>
-                        </tr>
-                    @endfor
-                </tbody>            
-            </table>
+        <div class="container box">
+            <h3 align="center">HackeChiapas</h3><br />
+            <div class="panel panel-default">
+                <div class="panel-heading">Hackers</div>
+                <div class="panel-body">
+                    <div class="form-group">
+                        <input type="text" name="search" id="search" class="form-control" placeholder="Search Customer Data" />
+                    </div>
+
+                    <div class="table-responsive">
+                        <table class="table">
+                    
+                            <thead class="thead-dark" style="background-color: #212121">
+                                <td style="background-color: #00C853; color: #FAFAFA" colspan="3">Lista de hackers confirmados</td>
+                                <tr>
+                                <th scope="col" style="color: #FAFAFA"> id</th>
+                                <th scope="col" style="color: #FAFAFA" > name</th>
+                                <th scope="col" style="color: #FAFAFA"> email </th>
+                            </tr>
+                            </thead>
+                            <tbody id="TbodyHackers">
+
+                            </tbody>            
+                        </table>
+                        <table class="table" style="margin-top: 50px;">
+                    
+                            <thead class="thead-dark" style="background-color: #212121">
+                                <td style="background-color: #D50000; color: #FAFAFA" colspan="3">Lista de hackers no confirmados</td>
+                                <tr>
+                                <th scope="col" style="color: #FAFAFA"> id</th>
+                                <th scope="col" style="color: #FAFAFA" > name</th>
+                                <th scope="col" style="color: #FAFAFA"> email </th>
+                            </tr>
+                            </thead>
+                            <tbody id="TbodyNotHackers">
+
+                            </tbody>            
+                        </table>
+                        
+                    </div>
+                </div>    
+            </div>
         </div>
-    </div>
-</div>
+    </body>
+</html>
+
+
 @endsection
