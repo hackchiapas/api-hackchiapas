f3652ddfaebaad76ed787ba566a14c9a610951b6
AugustoRuizC <163191@ids.upchiapas.edu.mx>
2018-09-01 19:06:58 -0500

    Otros cambios

---
+++ public/js/search.js
@@ -0,0 +1,5 @@
+var search = document.getElementById("search").value;
+var element = document.getElementById(search);
+
+console.log("Buscar: " + element);
+//console.log("Top position: " + search.top + " Left position: " + search.left);
